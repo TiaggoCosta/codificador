1d5ffee7f440de927c25a998f7c16610cdac315b
Karolina <karolina_cris@hotmail.com>
2020-08-16 22:52:40 -0300

    add unary codification

---
+++ codificacoes/unaria/UnaryCodification.java
@@ -0,0 +1,17 @@
+package codificacoes.unaria;
+
+import codificacoes.Decoder;
+import codificacoes.Encoder;
+
+public class UnaryCodification implements Encoder, Decoder {
+    
+    @Override
+    public void decode() {
+
+    }
+
+    @Override
+    public void encode() {
+
+    }
+}
