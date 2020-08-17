2767c138bcc54de271bfd5bf39eafd941aff7663
Tiago Costa <tiago.tagcosta@gmail.com>
2020-08-17 16:16:00 -0300

    escolha de função com opções definidas

--- Main.java
+++ Main.java
@@ -15,7 +15,14 @@ public class Main {
             op = JOptionPane.showOptionDialog(null, "Escolha a função desejada:", "Função", JOptionPane.DEFAULT_OPTION,
             JOptionPane.INFORMATION_MESSAGE, null, functions, functions[0]);
             
-            System.out.println("op: " + op);
+            if(op == 0) {
+                System.out.println("Codificar: " + op);
+            } else if(op == 1) {
+                System.out.println("Decodificar: " + op);
+            } else {
+                isOn = false;
+                break;
+            }
             
             // seleção de arquivo
             final JFileChooser fileChooser = new JFileChooser();
@@ -26,12 +33,15 @@ public class Main {
                 File selectedFile = fileChooser.getSelectedFile();
                 JOptionPane.showMessageDialog(frame, selectedFile.getName());
             }
+
+            System.out.println("retVal: " + retVal); // APPROVE_OPTION = 0 : CANCEL = 1
             
             // escolher codificador (0: Golomb, 1:Elias-Gamma, 2:Fibonacci, 3:Unária e 4:Delta)
             Object[] items = { "Golomb", "Elias-Gamma", "Fibonacci", "Unária", "Delta" };
             Object selectedValue = JOptionPane.showInputDialog(null, "Escolha um codificador:", "Opção",
                 JOptionPane.INFORMATION_MESSAGE, null, items, items[0]);
-            System.out.println("cod: " + selectedValue);
+
+            System.out.println("cod: " + selectedValue); // CANCEL = null
 
             if(op == 1) {
                 isOn = false;
