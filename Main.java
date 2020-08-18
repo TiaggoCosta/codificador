2d1fd3aa3a6593d7e812be3806d7fe1bb6b9851c
Tiago Costa <tiago.tagcosta@gmail.com>
2020-08-17 21:16:57 -0300

    adicionado input para divisor golomb

--- Main.java
+++ Main.java
@@ -10,6 +10,7 @@ import codificacoes.Decoder;
 public class Main {
  
     public static void main(String[] args) {
+        Map<String, Integer> identifiers = new HashMap<String, Integer>();
         Boolean isOn = true;
 
         while(isOn) {
@@ -22,7 +23,9 @@ public class Main {
                 System.out.println("Codificar: " + op);
             } else if(op == 1) {
                 System.out.println("Decodificar: " + op);
-            } else {
+            } 
+            if(op == -1) {
+                System.out.println("Finalizar: " + op);
                 Object[] options = { "Sim, finalizar programa", "Não, desejo recomeçar" };
                 int end = JOptionPane.showOptionDialog(null, "Deseja encerrar o programa?", "Finalizar",
                 JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
@@ -46,35 +49,43 @@ public class Main {
                 JOptionPane.showMessageDialog(null, selectedFile.getName());
             }
 
-            if(retVal == 0) {
-                System.out.println("Open: " + retVal);
-            } else if(retVal == 1) {
+            if(retVal == 1) {
                 System.out.println("Cancel/close: " + retVal);
                 continue;
-            } 
+            } else if(retVal == 0) {
+                System.out.println("Open: " + retVal);
+            }
             
-            // escolher codificador (0: Golomb, 1:Elias-Gamma, 2:Fibonacci, 3:Unária e 4:Delta)
-            static Map<String, Integer> identifiers = new HashMap<String, Integer>();
-            identifiers.put("Golomb", 0); identifiers.put("Elias-Gamma", 1); identifiers.put("Fibonacci", 2); identifiers.put("Unária", 3); identifiers.put("Delta", 4);
-            Object[] items = { "Golomb", "Elias-Gamma", "Fibonacci", "Unária", "Delta" };
-            Object selectedValue = JOptionPane.showInputDialog(null, "Escolha um codificador:", "Opção",
-                JOptionPane.INFORMATION_MESSAGE, null, items, items[0]);
-            
-            int divisor = 0;
-            // se golomb, informar o divisor
-
-            if(selectedValue == null) {
-                System.out.println("Close: " + selectedValue);
-                continue;
-            } else {
-                System.out.println("Selected Value: " + selectedValue);
-                if(op == 0) {
-                    Encoder encoder = new Encoder(selectedFile,identifiers.get(selectedValue), divisor);
+            if(op == 0) {
+                // escolher codificador (0: Golomb, 1:Elias-Gamma, 2:Fibonacci, 3:Unária e 4:Delta)
+                identifiers.put("Golomb", 0); identifiers.put("Elias-Gamma", 1); identifiers.put("Fibonacci", 2); identifiers.put("Unária", 3); identifiers.put("Delta", 4);
+                Object[] items = { "Golomb", "Elias-Gamma", "Fibonacci", "Unária", "Delta" };
+                Object selectedValue = JOptionPane.showInputDialog(null, "Escolha um codificador:", "Opção",
+                    JOptionPane.INFORMATION_MESSAGE, null, items, items[0]);
+                
+                int divisor = 0;
+                // se golomb, informar o divisor
+                if(selectedValue == "Golomb") {
+                    String inputValue = JOptionPane.showInputDialog("Insira o valor do divisor: ");
+                    try {
+                        divisor = Integer.parseInt(inputValue);
+                    } catch (Exception e) {
+                        //TODO: handle exception
+                    }
                 }
-                if(op == 1) {
-                    Decoder decoder = new Decoder(selectedFile);
+
+                if(selectedValue == null) {
+                    System.out.println("Close: " + selectedValue);
+                    continue;
+                } else {
+                    //Encoder encoder = new Encoder(selectedFile,identifiers.get(selectedValue), divisor);
+                    System.out.println("Encoder: " + selectedFile.getPath() + " codificador: " + identifiers.get(selectedValue) + " divisor: " + divisor);
                 }
             }
+            if(op == 1) {
+                //Decoder decoder = new Decoder(selectedFile);
+                System.out.println("Decoder: " + selectedFile.getPath());
+            }  
         }
         System.exit(0);
     }
