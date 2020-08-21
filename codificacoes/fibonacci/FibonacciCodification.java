06f68e5ba09e6cbf197972d09acc0e22490d285f
Rafael Baptista <rafael.stefani.baptista@gmail.com>
2020-08-20 22:06:46 -0300

    Fibonacci logic

--- codificacoes/fibonacci/FibonacciCodification.java
+++ codificacoes/fibonacci/FibonacciCodification.java
@@ -4,6 +4,7 @@ import codificacoes.Decoder;
 import codificacoes.Encoder;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class FibonacciCodification implements Encoder, Decoder {
 
@@ -19,11 +20,56 @@ public class FibonacciCodification implements Encoder, Decoder {
 
     @Override
     public byte[] encode(byte [] data) {
-        ArrayList<Byte> result = new ArrayList<>();
+        ArrayList<Byte> resultBytes = new ArrayList<>();
         for (byte b : data) {
+            byte resultByte = 0;
+            int bitPosition = 0;
             int value = Byte.toUnsignedInt(b);
+            ArrayList<Integer> fibonacciNumbers = getFibonacciNumbers(value);
+            fibonacciNumbers.sort(Collections.reverseOrder());
 
+            for (Integer i : fibonacciNumbers) {
+                if (bitPosition >= 7) {
+                    resultBytes.add(resultByte);
+                    resultByte = 0;
+                    bitPosition = 0;
+                }
+
+                if (value - i >= 0) {
+                    //resultByte add 1
+                } else {
+                    //resultByte add 0
+                }
+
+                bitPosition++;
+            }
+            //resultByte add stopbit (1)
+            resultBytes.add(resultByte);
         }
-        return data;
+
+        byte[] result = new byte[resultBytes.size()];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = resultBytes.get(i);
+        }
+
+        return result;
     }
+
+    public ArrayList<Integer> getFibonacciNumbers(int value) {
+        ArrayList<Integer> fibonacciNumbers = new ArrayList<>();
+        int currentNumber = 1;
+        int nextNumber = 2;
+        fibonacciNumbers.add(currentNumber);
+
+        while (nextNumber <= value) {
+            fibonacciNumbers.add(nextNumber);
+            int temp = currentNumber;
+            currentNumber = nextNumber;
+            nextNumber = temp + nextNumber;
+        }
+
+        return fibonacciNumbers;
+    }
+
 }
